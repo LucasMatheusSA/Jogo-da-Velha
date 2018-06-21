9ddf420c2f639af11ecf310dfb881591bd30f398
lucasx170 <36941834+lucasx170@users.noreply.github.com>
2018-06-21 13:14:08 -0300

    JODO DA VELHA

--- Jogo.java
+++ Jogo.java
@@ -25,6 +25,7 @@ public class Jogo {
                     break;
             }
         }while(opMenu!=0);
+        s.close();
     }
     
     public static void menuJogo(){
@@ -33,6 +34,7 @@ public class Jogo {
         int op=4;
         limparJogo(jogo);
         do{
+            System.out.println("---MENU---");
             System.out.println("1-Facil");
             System.out.println("2-Medio");
             System.out.println("3-Dificil");
@@ -57,57 +59,84 @@ public class Jogo {
         }while(op!=0);
     }
 
-    public static void jogador(int jogo[][],Long num){
+    public static void jogador(int jogo[][],int num){
         Scanner s = new Scanner(System.in);
         do{
-            switch(num){
-                case 1:jogo[2][0]=1;break;
-                case 2:jogo[2][1]=1;break;
-                case 3:jogo[2][2]=1;break;
-                case 4:jogo[1][0]=1;break;
-                case 5:jogo[1][1]=1;break;
-                case 6:jogo[1][2]=1;break;
-                case 7:jogo[0][0]=1;break;
-                case 8:jogo[0][1]=1;break;
-                case 9:jogo[0][2]=1;break;
-                default:System.out.println("jogada invalida!");num=s.nextInt();break;
+            if(num==10){
+                System.out.print("Posicao ocupada! Digite novamente:");num=s.nextInt();
             }
-        }while(num<0 && num >9);
+            if(num==11){
+                System.out.print("Digite novamente:");num=s.nextInt();
+            }
+            switch(num){ 
+                case 1:if(jogo[2][0]==0)jogo[2][0]=1;else num=10;break;
+                case 2:if(jogo[2][1]==0)jogo[2][1]=1;else num=10;break;
+                case 3:if(jogo[2][2]==0)jogo[2][2]=1;else num=10;break;
+                case 4:if(jogo[1][0]==0)jogo[1][0]=1;else num=10;break;
+                case 5:if(jogo[1][1]==0)jogo[1][1]=1;else num=10;break;
+                case 6:if(jogo[1][2]==0)jogo[1][2]=1;else num=10;break;
+                case 7:if(jogo[0][0]==0)jogo[0][0]=1;else num=10;break;
+                case 8:if(jogo[0][1]==0)jogo[0][1]=1;else num=10;break;
+                case 9:if(jogo[0][2]==0)jogo[0][2]=1;else num=10;break;
+                default:System.out.println("jogada invalida!");num=11;break;
+            }
+        }while(num<0 || num >9);
     }
 
     public static void aleatorio(int jogo[][]){
-        Random r = new Random(4);
-        int x=r.nextInt(4),y=r.nextInt(4);
+        Random r = new Random();
+        int x=r.nextInt(3),y=r.nextInt(3);
         while(jogo[x][y]!=0){
-            x=r.nextInt(4);
-            y=r.nextInt(4);
+            x=r.nextInt(3);
+            y=r.nextInt(3);
         }
         jogo[x][y]=2;
     }
 
     public static void fase1(int jogo[][]){
-        Random r = new Random(2);
         Scanner s = new Scanner(System.in);
-        Long num=r.nextInt(2);
-        if(num==1){
+        Random r = new Random();
+        int num=0;
+        int x=r.nextInt(2);
+        if(x==1){
             System.out.println("Jogador comeca jogando");
-            do{
+            while(gameOver(jogo)==0){
                 printJogo(jogo);
                 System.out.print("Digite sua jogada:");
                 num=s.nextInt();
                 jogador(jogo, num);
+                if(gameOver(jogo)!=0){
+                    break;
+                }
                 aleatorio(jogo);
-            }while(gameOver(jogo)==0);
+                limpar();
+            }
         }else{
             System.out.println("Computador comeca jogando");
-            do{
+            while(gameOver(jogo)==0){
                 aleatorio(jogo);
+                if(gameOver(jogo)!=0){
+                    break;
+                }
                 printJogo(jogo);
                 System.out.print("Digite sua jogada:");
                 num=s.nextInt();
                 jogador(jogo, num);
-            }while(gameOver(jogo)==0);
+                limpar();
+            }
         }
+        limpar();
+        if(gameOver(jogo)==1){
+            System.out.println("PARABENS JOGADOR!");
+            printJogo(jogo);
+        }else if(gameOver(jogo)==2){
+            System.out.println("COMPUTADOR E VENCEU!");
+            printJogo(jogo);
+        }else{
+            System.out.println("EMPATE!!!");
+            printJogo(jogo);
+        }
+        limparJogo(jogo);
     }
 
     public static void limparJogo(int jogo[][]){
@@ -138,6 +167,9 @@ public class Jogo {
         if((jogo[0][0]==2 && jogo[1][0]==2 && jogo[2][0]==2) || (jogo[0][1]==2 && jogo[1][1]==2 && jogo[2][1]==2) || (jogo[0][2]==2 && jogo[1][2]==2 && jogo[2][2]==2)){
             return 2;
         }
+        if(jogo[2][0]!=0 && jogo[2][1]!=0 && jogo[2][2]!=0 && jogo[1][0]!=0 && jogo[1][1]!=0 && jogo[1][2]!=0 
+                && jogo[0][0]!=0 && jogo[0][1]!=0 && jogo[0][2]!=0)
+                return 3;
 		return 0;
     }
 
